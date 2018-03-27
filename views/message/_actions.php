4bdd1e260598a48cc699e37a5aeead23856ed2d4
Herbert Maschke <thyseus@gmail.com>
2018-03-27 12:51:35 +0200

    tiny UX improvements

--- views/message/_actions.php
+++ views/message/_actions.php
@@ -1,10 +1,22 @@
 <?php
 
+/* @var $this \yii\web\View */
+
 use yii\helpers\Html;
 
 $action = Yii::$app->controller->action->id;
 
 ?>
+
+<?php
+if ($action == 'compose') {
+    $caption = '<i class="fa fa-plus"></i> <strong>' . Yii::t('message', 'Write a message') . '</strong>';
+} else {
+    $caption = '<i class="fa fa-plus"></i> ' . Yii::t('message', 'Write a message');
+} ?>
+
+<?= Html::a($caption, ['compose'], ['class' => 'btn btn-success']) ?>
+
 
 <?php
 if ($action == 'inbox') {
@@ -24,28 +36,47 @@ if ($action == 'sent') {
 
 <?= Html::a($caption, ['sent'], ['class' => 'btn btn-success']) ?>
 
+<?php
+if ($action == 'drafts' || isset($_GET['hash'])) {
+    $caption = '<i class="fa fa-file-text-o"></i> <strong>' . Yii::t('message', 'Manage Drafts') . '</strong>';
+} else {
+    $caption = '<i class="fa fa-file-text-o"></i> ' . Yii::t('message', 'Manage Drafts');
+} ?>
+
+<?= Html::a($caption, ['drafts'], ['class' => 'btn btn-success']) ?>
+
+<?php
+if ($action == 'templates' || isset($_GET['hash'])) {
+    $caption = '<i class="fa fa-file-text-o"></i> <strong>' . Yii::t('message', 'Manage Templates') . '</strong>';
+} else {
+    $caption = '<i class="fa fa-file-text-o"></i> ' . Yii::t('message', 'Manage Templates');
+} ?>
+
+<?= Html::a($caption, ['templates'], ['class' => 'btn btn-success']) ?>
+
+&nbsp;
 <div class="btn-group">
     <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
         <?= Yii::t('message', 'More'); ?>&nbsp;<span class="caret"></span>
     </button>
     <ul class="dropdown-menu" role="menu">
-        <?php
-        if ($action == 'drafts' || isset($_GET['hash'])) {
-            $caption = '<i class="fa fa-file-text-o"></i> <strong>' . Yii::t('message', 'Manage Drafts') . '</strong>';
-        } else {
-            $caption = '<i class="fa fa-file-text-o"></i> ' . Yii::t('message', 'Manage Drafts');
-        } ?>
-
-        <li> <?= Html::a($caption, ['drafts'], ['class' => 'btn btn-success']) ?> </li>
 
         <?php
-        if ($action == 'templates' || isset($_GET['hash'])) {
-            $caption = '<i class="fa fa-file-text-o"></i> <strong>' . Yii::t('message', 'Manage Templates') . '</strong>';
+        if ($action == 'manage-draft' && !isset($_GET['hash'])) {
+            $caption = '<i class="fa fa-plus"></i> <strong>' . Yii::t('message', 'Create a draft') . '</strong>';
         } else {
-            $caption = '<i class="fa fa-file-text-o"></i> ' . Yii::t('message', 'Manage Templates');
+            $caption = '<i class="fa fa-plus"></i> ' . Yii::t('message', 'Create a draft');
         } ?>
 
-        <li> <?= Html::a($caption, ['templates'], ['class' => 'btn btn-success']) ?> </li>
+        <li> <?= Html::a($caption, ['manage-draft'], ['class' => 'btn btn-success']) ?> </li>
+        <?php
+        if ($action == 'manage-template' && !isset($_GET['hash'])) {
+            $caption = '<i class="fa fa-plus"></i> <strong>' . Yii::t('message', 'Create a template') . '</strong>';
+        } else {
+            $caption = '<i class="fa fa-plus"></i> ' . Yii::t('message', 'Create a template');
+        } ?>
+
+        <li> <?= Html::a($caption, ['manage-template'], ['class' => 'btn btn-success']) ?> </li>
 
         <?php
         if ($action == 'signature') {
@@ -66,39 +97,6 @@ if ($action == 'sent') {
         <li> <?= Html::a($caption, ['ignorelist'], ['class' => 'btn btn-success']) ?> </li>
 
     </ul>
-</div>
-
-<div class="btn-group pull-right">
-    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-        <i class="fa fa-plus"> </i><span class="caret"></span>
-    </button>
-    <ul class="dropdown-menu" role="menu">
-        <?php
-        if ($action == 'manage-draft' && !isset($_GET['hash'])) {
-            $caption = '<i class="fa fa-plus"></i> <strong>' . Yii::t('message', 'Create a draft') . '</strong>';
-        } else {
-            $caption = '<i class="fa fa-plus"></i> ' . Yii::t('message', 'Create a draft');
-        } ?>
-
-        <li> <?= Html::a($caption, ['manage-draft'], ['class' => 'btn btn-success']) ?> </li>
-        <?php
-        if ($action == 'manage-template' && !isset($_GET['hash'])) {
-            $caption = '<i class="fa fa-plus"></i> <strong>' . Yii::t('message', 'Create a template') . '</strong>';
-        } else {
-            $caption = '<i class="fa fa-plus"></i> ' . Yii::t('message', 'Create a template');
-        } ?>
-
-        <li> <?= Html::a($caption, ['manage-template'], ['class' => 'btn btn-success']) ?> </li>
-
-        <?php
-        if ($action == 'compose') {
-            $caption = '<i class="fa fa-plus"></i> <strong>' . Yii::t('message', 'Write a message') . '</strong>';
-        } else {
-            $caption = '<i class="fa fa-plus"></i> ' . Yii::t('message', 'Write a message');
-        } ?>
-
-        <li> <?= Html::a($caption, ['compose'], ['class' => 'btn btn-success']) ?> </li>
-
 </div>
 
 <?php if ($action == 'inbox') { ?>
